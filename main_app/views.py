3fd25f524bf4576037ff594b5200342af0d6088e
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-03 14:08:25 -0600

    work on user and login functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-  return HttpResponse('<h1>Home Page</h1>')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return HttpResponse('<h1>About</h1>')
