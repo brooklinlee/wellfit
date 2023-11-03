a0767b716c65deac69d7cd8640cee54fce9c2f2e
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-03 14:21:13 -0600

    work on login functionality

--- wellfit/settings.py
+++ wellfit/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '' #UPDATE THIS LATER
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
