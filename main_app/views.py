d4aa85ddc564a56480c940acadc89839c37b423f
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-03 14:27:56 -0600

    set up authorization and login

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# PROTECT ROUTES: 
+# def (view functions): @login_required
+# class(class-based views): LoginRequiredMixin
 
 # Create your views here.
 class Home(LoginView):
@@ -8,3 +16,18 @@ class Home(LoginView):
 
 def about(request):
   return HttpResponse('<h1>About</h1>')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
+
